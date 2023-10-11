dbd280fe051d5e33bf4216083fa9b98198eee209
Алексей Суслов <suslovwow@yandex.ru>
2023-10-11 19:13:47 +0300

    решил задачу 1

--- task001/Program.cs
+++ task001/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Введено нетрёхзначное число!");
+}
+else
+{
+    i = (number - number / 100 * 100) / 10;
+    Console.WriteLine(i);
+}
