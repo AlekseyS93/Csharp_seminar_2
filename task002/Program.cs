cc9086260ec778b600129c05a7944332882c4668
Алексей Суслов <suslovwow@yandex.ru>
2023-10-11 20:35:47 +0300

    решил задачу 3

--- task002/Program.cs
+++ task002/Program.cs
@@ -2,20 +2,26 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 326791123 -> 6
+//#include <cmath>
 
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int i = 0;
+// if (number < 0)
+// {
+//     number = number * -1;
+// }
+// хотел сделать для отрицательного числа, но не получилось сделать модуль =(((
 if (number < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
 else
 {
-while (number > 999)
-{
-    number = number/10;
-}
-i = number%10;
-Console.WriteLine(i);
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    i = number % 10;
+    Console.WriteLine(i);
 }
---
+++ task003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите день недели от 1 до 7 и узнайте выходной ли он: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0 || number > 7)
+{
+    Console.WriteLine("такого дня недели нет");
+}
+else if (number >= 1 && number <= 5)
+{
+    Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("ДА!");
+}
